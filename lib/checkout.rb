d16eb685a9c2beddd8989d8d24d5e3a0b2a9da20
Alberto Paz <alberto.pazjimenez@gmail.com>
2025-07-15 16:37:10 +0200

    Returns the correct total for a basket with one item

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,5 +1,25 @@
 class Checkout
   def total
-    0
+    items.sum(&:price)
+  end
+
+  def add_item(product_code:, name:, price:)
+    items << Item.new(product_code: product_code, name: name, price: price)
+  end
+
+  class Item
+    attr_reader :product_code, :name, :price
+
+    def initialize(product_code:, name:, price:)
+      @product_code = product_code
+      @name = name
+      @price = price
+    end
+  end
+
+  private
+
+  def items
+    @items ||= []
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,3 +1,4 @@
+require "rspec"
 require_relative "../lib/checkout"
 
 RSpec.describe Checkout do
@@ -5,5 +6,10 @@ RSpec.describe Checkout do
 
   it "has a total of 0 for an empty basket" do
     expect(subject.total).to eq(0)
+  end
+
+  it "returns the correct total for a basket with one item" do
+    subject.add_item(product_code: "GR1", name: "Green Tea", price: 311)
+    expect(subject.total).to eq(311)
   end
 end
